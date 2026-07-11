f05c8c92307e2d8bdaa145726667811891179c89
uzuto <imsidk2004@gmail.com>
2026-07-11 14:47:29 +0530

    mpv: renamed script

--- mpv/.config/mpv/scripts/signal-ratio.lua
+++ mpv/.config/mpv/scripts/signal-ratio.lua
@@ -1,9 +1,9 @@
 function check_chapter_timeline()
     local title = mp.get_property("chapter-metadata/title")
     if title then
-        if string.find(title, "%[PAST%]") then
+        if string.find(title, "PAST") then -- "%[PAST%]"
             mp.set_property("video-aspect-override", "2.35:1")
-        elseif string.find(title, "%[PRESENT%]") then
+        elseif string.find(title, "PRESENT") then -- "%[PRESENT%]"
             mp.set_property("video-aspect-override", "16:9")
         end
     end
